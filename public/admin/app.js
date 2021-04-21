73b2d797866e1eacb9019971752c031c1ab82554
Ijas <17mcs22@meaec.edu.in>
2021-04-21 18:27:32 +0530

    first commit

---
+++ public/admin/app.js
@@ -0,0 +1,19 @@
+var adminApp = angular.module('ghost-clone.admin', [
+    'ui.router',
+    'btford.markdown'
+]);
+
+adminApp.config(function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('allPosts', {
+            url: '/',
+            templateUrl: '/admin/templates/allPosts.html'
+        })
+        .state('addPost', {
+            url: '/addPost',
+            templateUrl: '/admin/templates/addPost.html'
+        })
+});
